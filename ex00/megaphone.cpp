41b9ad30f2f48171c40d6ec6947370a9e1387e43
lle-briq <lle-briq@student.42.fr>
2021-10-19 19:58:16 +0200

    ex00 ok

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,16 +6,38 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 17:20:04 by lle-briq          #+#    #+#             */
-/*   Updated: 2021/10/19 17:23:18 by lle-briq         ###   ########.fr       */
+/*   Updated: 2021/10/19 19:54:50 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+void    print_upper(char *str)
+{
+    int j;
+
+    j = 0;
+    while (str[j])
+    {
+        if (str[j] >= 'a' && str[j] <= 'z')
+            std::cout << (char)(str[j] - 'a' + 'A');
+        else
+            std::cout << str[j];
+        j++;
+    }
+}
+
 int main(int argc, char **argv)
 {
+    int i;
+
     if (argc == 1)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
     }
+    i = 0;
+    while (++i < argc)
+        print_upper(argv[i]);
+    std::cout << std::endl;
 }
--- notes.md
+++ notes.md
@@ -1,2 +1,12 @@
 # Notes videos C++
 ## Namespace
+```
+namespace name {
+    int var1;
+    void *function1 ...
+}
+```
+cree un ensemble. Un peu comme des structures en C.
+
+## Streams
+std::cout std::cin
